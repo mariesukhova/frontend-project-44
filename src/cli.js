a32b83c5cddb831c09d818ddd909593dc8773144
Maria Sukhova <mariesukhova@gmail.com>
2022-11-01 19:02:01 +0300

    add greetings function

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+  console.log("May I have your name?");
+  const name = readlineSync.question('Your answer: ');
+  console.log("Hello, " + name + "!");
+}
+
+export default greetings;
