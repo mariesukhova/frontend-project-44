3876c72d222ca61aa53a1d22b58029c795c88417
Maria Sukhova <mariesukhova@gmail.com>
2023-04-21 01:05:03 +0300

    [step-refactoring-v.2] - Refactor games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,4 @@
 // eslint-disable-next-line import/extensions
 import greetings from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
 greetings();
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import game from '../src/game.js';
+import generateRandomNum from '../src/generateRandomNum.js';
 
-function getGcdTwoNumbers(randomNumber1, randomNumber2) {
-  let x = randomNumber1;
-  let y = randomNumber2;
+function getGcdTwoNumbers(num1, num2) {
+  let x = num1;
+  let y = num2;
   while (y) {
     const t = y;
     y = x % y;
@@ -11,36 +12,17 @@ function getGcdTwoNumbers(randomNumber1, randomNumber2) {
   }
   return x;
 }
-
-const gcd = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('May I have your name?');
-  const name = readlineSync.question('Your answer: ');
-  console.log(`Hello, ${name}!`);
-  console.log('Find the greatest common divisor of given numbers');
-
-  let i = 0;
-  while (i < 3) {
-    const randomNumber1 = Math.round(Math.random() * 20);
-    const randomNumber2 = Math.round(Math.random() * 20);
-    const expression = (`${randomNumber1} ${randomNumber2}`);
-
-    const gcdTwoNumbers = getGcdTwoNumbers(randomNumber1, randomNumber2);
-    console.log(`Question: ${expression}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === gcdTwoNumbers.toString()) {
-      i += 1;
-      if (i === 3) {
-        console.log(`Congratulations, ${name}!`);
-      } else {
-        console.log('Correct!');
-      }
-    } else if (answer !== gcdTwoNumbers) {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${gcdTwoNumbers}'.\nLet's try again, ${name}!`,
-      );
-      break;
-    }
-  }
+function generateExpression() {
+  const randomNumber1 = generateRandomNum(20);
+  const randomNumber2 = generateRandomNum(20);
+  return (`${randomNumber1} ${randomNumber2}`);
+}
+function generateRightAnswer(expression) {
+  const [randomNumber1, randomNumber2] = expression.split(' ');
+  const gcdTwoNumbers = getGcdTwoNumbers(randomNumber1, randomNumber2);
+  return gcdTwoNumbers.toString();
+}
+const brainGcd = () => {
+  game('Find the greatest common divisor of given numbers', generateExpression, generateRightAnswer);
 };
-gcd();
+brainGcd();
