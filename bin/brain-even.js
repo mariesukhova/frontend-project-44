f2b76f4b5c00ac9064fb22705b8d2b1e9a837ff8
Maria Sukhova <mariesukhova@gmail.com>
2023-04-21 12:27:37 +0300

    [step-last fix]

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,11 +2,15 @@
 import game from '../src/game.js';
 import generateRandomNum from '../src/generateRandomNum.js';
 
+function generateExpression() {
+  const randomNumber1 = generateRandomNum(10) + 1;
+  return `${randomNumber1}`;
+}
 function getRightAnswer(num) {
   return num % 2 === 0 ? 'yes' : 'no';
 }
 const brainEven = () => {
-  game('Answer "yes" if the number is even, otherwise answer "no".', generateRandomNum, getRightAnswer);
+  game('Answer "yes" if the number is even, otherwise answer "no".', generateExpression, getRightAnswer);
 };
 
 brainEven();
